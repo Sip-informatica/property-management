4dd61006ab734937b9734fa65970484b9f1f83ce
Candi <open.hardware.libre@gmail.com>
2021-01-21 19:39:13 +0000

    Implementar UserDetailsService y UserDetails

--- src/main/java/es/sipinformatica/propertymanagement/security/data/model/User.java
+++ src/main/java/es/sipinformatica/propertymanagement/security/data/model/User.java
@@ -24,33 +24,36 @@ public class User {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;    
+    private Long id; 
+    @NonNull
+    @Column(unique = true)
+    private String dni;   
     @NonNull
     @Column(unique = true, nullable = false)
     private String userName;
     @NonNull
     @Pattern(regexp = PASSWORD_PATTERN)
-    private String password;
-    private String firstName;
-    private String lastName;
+    private String password;    
     @NonNull
     @Email
     @Column(unique = true, nullable = false)
-    private String email;
-    private String phone;
-    private String address;
-    private String city;
-    private String country;
-    private LocalDateTime firstAccess;
-    private LocalDateTime lastAccess;
-    @NonNull
-    @Column(unique = true)
-    private String dni;
-    private Boolean active;
+    private String email;     
+    private Boolean isAccountNonExpired;
+    private Boolean isAccountNonLocked;
+    private Boolean isCredentialsNonExpired;
+    private Boolean isEnabled;
     @ManyToMany
     @JoinTable(
          name = "user_roles",
          joinColumns = @JoinColumn( name = "user_id" ),
          inverseJoinColumns = @JoinColumn( name= "role_id" ))
     private Set<Role> roles = new HashSet<>();
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private String city;
+    private String country;
+    private LocalDateTime firstAccess;
+    private LocalDateTime lastAccess;
 }
---
+++ src/main/java/es/sipinformatica/propertymanagement/security/domain/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package es.sipinformatica.propertymanagement.security.domain.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.sipinformatica.propertymanagement.security.api.dtos.UserDetailsImpl;
+import es.sipinformatica.propertymanagement.security.data.daos.UserRepository;
+import es.sipinformatica.propertymanagement.security.data.model.User;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+    
+}
